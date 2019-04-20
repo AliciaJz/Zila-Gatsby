f92a7e44d3b817dcacd8db03792e44accd3c9a4e
AliciaJz <aliciajuarez.dev@gmail.com>
2019-04-19 20:33:13 -0500

    Pi

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
   `)
   return (
     <footer className={footerStyles.footer}>
-      <p>Created by {data.site.siteMetadata.author}, © 2019</p>
+      <p>Created by {data.site.siteMetadata.author}, © 2019 π</p>
     </footer>
   )
 }
